9390b0eff04ea0807ccd934546c5f0f406a9ea7d
tomato <ljc6qx@163.com>
2023-08-12 16:41:27 +0800

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -18,6 +18,25 @@ if (command === ".dbinfo") {
   // Uncomment this to pass the first stage
   const pageSize = buffer.readUInt16BE(16); // page size is 2 bytes starting at offset 16
   console.log(`database page size: ${pageSize}`);
+
+  // buffer.readUInt16BE（16）从缓冲区中读取页面大小，console.log将其打印出来。if语句检查页面大小是否小于112（标头为100，最小所需数据为12），如果小于，则抛出错误。
+  if (pageSize < 100 + 12) {
+    throw `PageSize too mall to contain headers`
+  }
+
+  // 添加的代码行负责从数据库文件中读取sqlite_schema表头，并提取数据库中的表（单元格）数量。
+  // 首先，代码将从文件的第100个字节开始的8个字节读取到sqliteSchemaHeader中。这就是sqlite_schema表头所在的位置。
+  const { buffer: sqliteSchemaHeader } = await databaseFileHandler.read({
+    length: 8,
+    position: 100,
+    buffer: Buffer.alloc(8)
+  })
+
+  // 然后，它从sqliteSchemaHeader的第3个字节读取单元格（表）的数量，并将其记录到控制台。
+  const numCells = sqliteSchemaHeader.readUInt16BE(3)
+  console.log(`number of tables: ${numCells}`)
+
+  await databaseFileHandler.close()
 } else {
   throw `Unknown command ${command}`;
 }
